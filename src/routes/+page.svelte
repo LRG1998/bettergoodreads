5b282f128d65dbca3950e77e7b52ffbf81e72201
LRG1998 <ericwarfish212@gmail.com>
2023-03-14 14:51:57 -0500

    I managed to get a stylesheet to work.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../style/app.css'
+</script>
+
+
+<head>
+	<meta charset="utf-8" />
+    <link rel="icon" href="%sveltekit.assets%/favicon.png" />
+    <link rel = "stylesheet" href="style/app.css">
+    <meta name="viewport" content="width=device-width" />
+</head>
+
+<body data-sveltekit-preload-data="hover">
+    
+    <div class="header">
+        <h1>The Librarian</h1>
+        <h3>The internet librarian</h3>
+    </div>
+    <div class = "topbar">
+        <p>Text</p>
+        <input type="text" placeholder="Search...">
+        <button>Ask the librarian</button>
+    </div>
+    </body>
+

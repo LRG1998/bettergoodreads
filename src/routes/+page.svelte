2af65949230c33caf5cec5e5e384d63bf405f46a
LRG1998 <ericwarfish212@gmail.com>
2023-04-07 16:07:51 -0500

    Set up basic login, and broke user screen area.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,41 @@
-<script lang="ts">
-    import '../style/app.css'
-    
+<script>
+  import { onMount } from "svelte";
+    let loggedin = false;
+
+    onMount(() => {
+        let username = "";
+        if(loggedin == false) {
+            document.getElementById("login").hidden = false;
+            document.getElementById("user").hidden = true;
+        }
+        else{
+            document.getElementById("login").hidden = true;
+            document.getElementById("user").hidden = false;
+        }
+        
+    });
+    function logout(){
+        loggedin = false;
+        document.getElementById("login").hidden = false;
+        document.getElementById("user").hidden = true;
+    };
+    function login(username){
+        let userinput = document.getElementById("userinput")
+        let userpass = document.getElementById("pass")
+        if(userinput.value != ""){
+        username = userinput.value;
+        document.getElementById("username").innerText = username;
+        loggedin = true;
+        document.getElementById("login").hidden = true;
+        document.getElementById("user").hidden = false;
+        userinput.style.border="none";
+        userinput.value = "";
+        userpass.value = "";
+        } else{
+            userinput.style.border="red 1px solid"
+        }
+    };
 </script>
-
-
 <head>
 	<meta charset="utf-8" />
     <link rel="icon" href="%sveltekit.assets%/favicon.png" />
@@ -20,38 +52,45 @@
         <h2>
             Home
         </h2>
-
+        <div id = "login">
+            <p>User: <input id = "userinput" type="text"></p>
+            <p>Password: <input id="pass" type = "password"></p>
+            <button type="submit"on:click={()=>login()}>Log in</button>
+        </div>
         <div id = "user">
             <a href="./profile/">
             <img src="./images/default.png" alt="User Icon" id = "usericon">
             </a>
-            <p id = "username">%Username%</p><br>
+            <p id = "username">%Username%</p>
             <a href="#" id = "edit">Edit Info</a>
+            <button type="submit" on:click={()=>logout()}>Log off</button>
         </div>                
     </div>
             
 
     <div id = "topbar">
-        <a href="../about/">About</a>
-        <a href="#">Hover test</a>
+        <a id = "barnav" href="../about/">About</a>
+        <a id = "barnav" href="#">Hover test</a>
         <input type="text" placeholder="Search...">
-        <button id="SearchButton">Ask the librarian</button>
+        <a href="search/"><button id="SearchButton">Ask the librarian</button></a>
     </div>
-    <div id="content">
+    <div id= "content">
+    <div id="recommended">
         <p>
             test of the content border
         </p>
     </div>
-    <div id="content">
+    <div id="recommended">
         <p>
             test of the content border
         </p>
     </div>
-    <div id="content">
+    <div id="recommended">
         <p>
             test of the content border
         </p>
     </div>
+</div>
     </body>
     <footer>
         <p>
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,6 +1,3 @@
-
-
-
 <head>
 	<meta charset="utf-8" />
     <link rel="icon" href="%sveltekit.assets%/favicon.png" />
@@ -8,30 +5,31 @@
     <meta name="viewport" content="width=device-width" />
 </head>
 
-<body data-sveltekit-preload-data = "hover">
+<body data-sveltekit-preload-data="hover">
+    
     <div id="header">
-        <a id="title" href="../">
+        <a id="title" href="./">
             <img src = "./images/logo.png" alt="logo" id="logo">
         </a>
         <h2>
-            About
+            Search
         </h2>
-        <a id = "user" href="../profile">
+        <div id = "user">
+            <a href="./profile/">
             <img src="./images/default.png" alt="User Icon" id = "usericon">
+            </a>
             <p id = "username">%Username%</p><br>
             <a href="#" id = "edit">Edit Info</a>
-        </a>
+        </div>                
     </div>
+
     <div id = "topbar">
-        <a href="../about/" id = "active">About</a>
-        <a href="#">Hover test</a>
+        <a id = "barnav" href="../about/">About</a>
+        <a id = "barnav" href="#">Hover test</a>
         <input type="text" placeholder="Search...">
-        <button id="SearchButton">Ask the librarian</button>
+        <a href="search/"><button id="SearchButton">Ask the librarian</button></a>
     </div>
-    <div id = "body">
-        <p>
-            The Librarian is an alternative to goodreads which was built on the idea of providing a better alternative to that platform. <br>
-            The page as of writing is still under construction.
-        </p>
+    <div id = 'resultfield'>
+        Results
     </div>
 </body>
